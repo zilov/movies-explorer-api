80fc21ccc7ac6c88fefab0febba186ed4a85ed61
Danil Zilov <zilov.d@gmail.com>
2022-09-26 12:05:44 +0300

    feat: movies router

---
+++ routes/movies.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const {
+  getMovies, addMovie, deleteMovie
+} = require('../controllers/movies');
+
+router.get('/movies', getMovies);
+
+router.post('/movies', celebrate({
+  body: Joi.object().keys({
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/), // eslint-disable-line,
+    trailerLink: Joi.string().required().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/), // eslint-disable-line,
+    thumbnail: Joi.string().required().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/), // eslint-disable-line,
+    owner: Joi.string().hex().length(24).required(),
+    movieId: Joi.string().hex().length(24).required(),
+  }),
+}), addMovie);
+
+router.delete('/movies/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().hex().length(24).required(),
+  }),
+}), deleteMovie);
+
+module.exports = router;
