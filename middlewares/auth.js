6ae0910ab2a77f5f87b21b3aaf1bf1b70c89229e
Danil Zilov <zilov.d@gmail.com>
2022-09-27 12:25:12 +0300

    feat: token checker on auth

---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const Users = require('../models/user');
+const { NODE_ENV, JWT_SECRET }  = process.env;
+
+const {
+  UnauthorizedError,
+  NotFoundError,
+} = require('../controllers/errors');
+
+const checkToken = async (req, res, next) => {
+  const decoded = await jwt.verify(req.cookies.jwt,
+    NODE_ENV === 'production' ? JWT_SECRET : 'secret');
+  if (!decoded) {
+    return next(new UnauthorizedError('Cannot find JWT! Please sign in!'));
+  }
+  const user = await Users.findById(decoded)
+  if (!user) {
+    return next(new NotFoundError('Please sign in! Token is expired, cannot find user!'));
+  }
+  req.user = decoded;
+  return next();
+}
+
+module.exports = {
+  checkToken,
+};
