5bd0b2e9b53999fc4def5127512edb45d1d70893
Danil Zilov <zilov.d@gmail.com>
2022-11-15 02:59:16 +0300

    fix: 401 error on wrong token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -14,19 +14,23 @@ const checkToken = async (req, res, next) => {
   if (!req.cookies.jwt) {
     return next(new UnauthorizedError(errorMessages.jwtNotFound));
   }
-  const decoded = await jwt.verify(
-    req.cookies.jwt,
-    NODE_ENV === 'production' ? JWT_SECRET : jwtSecretDevelopment,
-  );
-  if (!decoded) {
+  try {
+    const decoded = await jwt.verify(
+      req.cookies.jwt,
+      NODE_ENV === 'production' ? JWT_SECRET : jwtSecretDevelopment,
+    );
+    if (!decoded) {
+      return next(new UnauthorizedError(errorMessages.jwtNotFound));
+    }
+    const user = await Users.findById(decoded);
+    if (!user) {
+      return next(new NotFoundError(errorMessages.expiredToken));
+    }
+    req.user = user;
+    return next();
+  } catch (e) {
     return next(new UnauthorizedError(errorMessages.jwtNotFound));
   }
-  const user = await Users.findById(decoded);
-  if (!user) {
-    return next(new NotFoundError(errorMessages.expiredToken));
-  }
-  req.user = user;
-  return next();
 };
 
 module.exports = {
